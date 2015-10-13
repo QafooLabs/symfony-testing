a58cf0dac126f953e28429a303cf342d7d51a148
Benjamin Eberlei <kontakt@beberlei.de>
2015-10-13 11:57:16 +0200

    Update Testing app

--- features/hello.feature
+++ features/hello.feature
@@ -1,5 +1,5 @@
 Feature: Hello World
 
-    Scenario: Hello Fabien
+    Scenario: Welcome Symfony
         Given I am on "/"
         Then I should see "Welcome to Symfony"
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,8 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
@@ -17,5 +19,44 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
         ));
+    }
+
+    /**
+     * @Route("/hello")
+     */
+    public function helloAction(Request $request)
+    {
+        if ($request->getMethod() === 'POST') {
+            return $this->redirect(
+                $this->generateUrl(
+                    'hello',
+                    ['name' => $request->request->get('name')]
+                )
+            );
+        }
+
+        return new Response(<<<HTML
+<form method="POST" action="/hello">
+    <input name="name"/>
+    <input type="submit" value="Submit" />
+</form>
+HTML
+        );
+    }
+
+    /**
+     * @Route("/hello/{name}", name="hello")
+     */
+    public function helloNameAction($name)
+    {
+        return new Response('Hello ' . $name);
+    }
+
+    /**
+     * @Route("/hello/{name}.json")
+     */
+    public function helloJsonAction($name)
+    {
+        return new JsonResponse(['Hello' => $name]);
     }
 }
